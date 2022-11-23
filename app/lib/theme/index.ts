072962d8d4ad90c2238e559c6c1d0525895e53a3
Giovani Silva <giovani.m.silva@gmail.com>
2022-11-23 17:01:04 -0300

    Add e2e tests

--- app/lib/theme/index.ts
+++ app/lib/theme/index.ts
@@ -18,4 +18,18 @@ export const theme = extendTheme({
       baseStyle: { text: { mt: 1 } },
     },
   },
+  colors: {
+    primary: {
+      50: '#FFE5E5',
+      100: '#FFB8B8',
+      200: '#FF8A8A',
+      300: '#FF5C5C',
+      400: '#FF2E2E',
+      500: '#FF0000',
+      600: '#CC0000',
+      700: '#990000',
+      800: '#660000',
+      900: '#330000'
+    }
+  }
 });
